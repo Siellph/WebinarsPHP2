35b68e90df5f230725282ccb8cf46da4c4226381
Vladislav <55257184+Siellph@users.noreply.github.com>
2020-05-25 17:16:06 +0500

    install composer+twig

---
+++ lesson3/vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+      0 => '9999999-dev',
+    ),
+    'reference' => 'b3ad39c73d3f9d6efceef275320b7fb80b07537f',
+    'name' => 'vendor_name/package_name',
+  ),
+  'versions' => 
+  array (
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.17.0',
+      'version' => '1.17.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fa79b11539418b02fc5e1897267673ba2c19419c',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2a86dde1288d7270169083d0e078dc7ebe0f48b6',
+    ),
+    'vendor_name/package_name' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'b3ad39c73d3f9d6efceef275320b7fb80b07537f',
+    ),
+  ),
+);
