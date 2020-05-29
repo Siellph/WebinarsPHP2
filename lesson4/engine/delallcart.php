db2975e3ab301d4edd67744895b50fca186ada42
Vladislav <55257184+Siellph@users.noreply.github.com>
2020-05-29 16:32:16 +0500

    delete

--- lesson4/engine/delallcart.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "database.php";
-$id_user = $_POST['user_id'];
-$sql = "DELETE FROM `cart` WHERE `user_id` = '$id_user'";
-$res = mysqli_query($connection, $sql);
-mysqli_close($connection);
-exit('<meta http-equiv="refresh" content="0; url=../cart.php" />');
-?>
