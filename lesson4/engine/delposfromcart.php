eff148876ddfc08683e8dc15bb501ae9a4e8daa3
Vladislav <55257184+Siellph@users.noreply.github.com>
2020-05-30 21:25:21 +0500

    -

--- lesson4/engine/delposfromcart.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "database.php";
-$pos_id = $_POST['pos_id'];
-$sql = "DELETE FROM `cart` WHERE `id` = '$pos_id'";
-$res = mysqli_query($connection, $sql);
-exit('<meta http-equiv="refresh" content="0; url=../cart.php" />');
-mysqli_close($connection);
-print_r($sql);
-?>
--- lesson4/engine/exit.php
+++
@@ -1,9 +0,0 @@
-<?php
-$login = $_COOKIE['login'];
-$pass = $_COOKIE['pass'];
-
-setcookie("login",$login,time()-7200,'/');
-setcookie("pass",$pass,time()-7200,'/');
-
-header("Location: ../index.php");
-?>
